0f1ce105414472d17a37b0eac918aa0b54b8ed9b
devhamid2002 <hamid.yavarzadeh2002@gmail.com>
2026-09-07 20:04:01 +0330

    feat: create Providers component to manage theme context using next-themes

---
+++ src/components/providers/Providers.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from "next-themes";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Providers({ children }: Props) {
+  return (
+    <>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        >
+        {children}
+        </ThemeProvider>
+    </>
+  );
+}
+
